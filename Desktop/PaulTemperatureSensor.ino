1cf06d99ec78e275909c28fe7d6fdd956f048df8
Paul Tshilenge <s00124602@e2009-01.campus.itsligo.ie>
2017-03-22 16:04:12 +0000

    sensor

---
+++ Desktop/PaulTemperatureSensor.ino
@@ -0,0 +1,67 @@
+/*
+  Hello World.ino
+  2013 Copyright (c) Seeed Technology Inc.  All right reserved.
+
+  Author:Loovee
+  2013-9-18
+
+  Grove - Serial LCD RGB Backlight demo.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
+
+void setup() 
+{
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    
+    lcd.setRGB(colorR, colorG, colorB);
+    
+    // Print a message to the LCD.
+    lcd.print("hello, world!");
+
+    delay(1000);
+}
+
+float temperature;
+int B=3975;                  //B value of the thermistor
+float resistance;
+//Serial.begin(9600);
+void loop() 
+{
+int a=analogRead(0);
+    resistance=(float)(1023-a)*10000/a; //get the resistance of the sensor;
+    temperature=1/(log(resistance/10000)/B+1/298.15)+273.15;//convert to temperature via datasheet ;
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1);
+    // print the number of seconds since reset:
+    lcd.print(temperature);
+
+    delay(100);
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
